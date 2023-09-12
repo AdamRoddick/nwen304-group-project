b926230a193efc192388f062f0dd87c21355ee88
Adam Roddick <adamroddick.developer@gmail.com>
2023-09-12 22:55:54 +1200

    Implement Posts to save locally and display dynamicly functionality

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,9 @@ window.addEventListener("load", init);
 
 function init() {
     bindEvents();
+
+    // Initialize the postOperations.posts array with posts from localStorage
+    postOperations.posts = JSON.parse(localStorage.getItem('posts')) || [];
 }
 
 let auto = autoGen();
@@ -10,19 +13,74 @@ function bindEvents() {
     document.querySelector('#post-button').addEventListener('click', addPost);
 }
 
-function addPost() {
+function addPost(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
     const title = document.querySelector('#post-title').value;
     const text = document.querySelector('#post-text').value;
-    const user = "John Smith" //Placeholder name
+    const user = "John Smith"; // Placeholder name
     const time = getCurrentTime();
     const id = generateUniqueId();
 
-    //Create a new post object and add to posts list
+    // Create a new post object
     const newPost = new Post(id, user, title, text, time);
+
+    // Add the new post to the array
     postOperations.add(newPost);
 
+    // Update the posts in localStorage
+    localStorage.setItem('posts', JSON.stringify(postOperations.posts));
 
+    // Display the new post on the website
+    displayPost(newPost);
+
+    // Clear input fields
+    document.querySelector('#post-title').value = '';
+    document.querySelector('#post-text').value = '';
 }
+
+
+function displayPost(post) {
+    const postList = document.querySelector('.post-list');
+
+    // Create a new post element
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+
+    // Construct the post HTML structure
+    postElement.innerHTML = `
+        <div class="post-header">
+            <img src="images/default-avatar.jpg" alt="User Profile Picture" class="post-profile-picture">
+            <div class="post-header-info">
+                <h3 class="post-username">${post.user}</h3>
+                <p class="post-time">${post.time}</p>
+            </div>
+        </div>
+        <div class="post-content">
+            <h3 class="post-title">${post.title}</h3>
+            <p class="post-text">${post.text}</p>
+        </div>
+    `;
+
+    // Add the new post to the post list
+    postList.appendChild(postElement);
+}
+
+// Function to initialize and display existing posts
+function displayExistingPosts() {
+    const postList = document.querySelector('.post-list');
+
+    // Loop through existing posts and display them
+    for (const post of postOperations.posts) {
+        displayPost(post);
+    }
+}
+
+// Call the function to display existing posts when the page loads
+window.addEventListener("load", () => {
+    init();
+    displayExistingPosts();
+});
 
 function getCurrentTime() {
     // Create a new Date object to get the current date and time
