eed34f0a0ffca2fe699e9bd89c3677b9921029e7
Adam Roddick <adamroddick.developer@gmail.com>
2023-09-12 22:08:50 +1200

    Implement function to fetch current time

--- js/controller.js
+++ js/controller.js
@@ -11,4 +11,19 @@ function bindEvents() {
 function addPost() {
     const title = document.querySelector('#post-title').value;
     const text = document.querySelector('#post-text').value;
+    const user = "John Smith" //Placeholder name
+    const time = getCurrentTime();
+}
+
+function getCurrentTime() {
+    // Create a new Date object to get the current date and time
+    var currentDate = new Date();
+    // Extract the current time components
+    var hours = currentDate.getHours();     // Get the current hour (0-23)
+    var minutes = currentDate.getMinutes(); // Get the current minute (0-59)
+    var seconds = currentDate.getSeconds(); // Get the current second (0-59)
+    // Format the time with leading zeros
+    var formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    // Return the formatted time as a string
+    return formattedTime;
 }
