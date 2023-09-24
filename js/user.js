aef676ea8c16d59c2bb9d1bbb3e0dfd08aef5418
Adam Roddick <adamroddick.developer@gmail.com>
2023-09-24 18:31:13 +1300

    Create user.js class

---
+++ js/user.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(id, firstName, lastName, email, password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
