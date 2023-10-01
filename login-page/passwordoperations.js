fbe7a59abf8fb13e635c0e32c64c4f2635f4a9cc
mcevoybenj <mcevoybenj@ecs.vuw.ac.nz>
2023-10-01 19:19:32 +1300

    Function to evaluate the password strength

--- login-page/passwordoperations.js
+++ login-page/passwordoperations.js
@@ -26,3 +26,48 @@ function checkRequirements(password) {
     }
     return meetsRequirements;
 }
+
+function evaluatePassword(password) {
+    
+    // fields to measure strength
+    var strength = 0;
+    var variations = 0;
+    const upperCase = /[A-Z]/;
+    const lowerCase = /[a-z]/;
+    const numbers = /\d/;
+    const symbols = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
+
+    // case there is no password
+    if(password == null){
+        return strength;
+    }
+
+    if (checkRequirements(password)) {
+
+        let requirementVariation = {
+            casesUpper: upperCase.test(password),
+            casesLower: lowerCase.test(password),
+            digitsUsed: numbers.test(password),
+            symbolsUsed: symbols.test(password) 
+        }
+
+        for (let i in requirementVariation) {
+            variations += (requirementVariation[i] == true) ? 1 : 0;
+        }
+
+        strength +=  (variations - 1) * 10; // 10 points for each variation (max 40)
+
+        // Increase strength level for every unique letter until 5 repetitions
+        let evalPassword = new Object();
+        for (let i = 0; i < password.length; i++) {
+            // Increments the count of the letter in the object
+            // If the letter has not been encountered, starts with 0
+            evalPassword[password[i]] = (evalPassword[password[i]] || 0) + 1; // checks whether the letter has been encountered before
+            // strength increases by 5 divided by the number of times the letter has been encountered
+            strength += 5.0 / evalPassword[password[i]]; 
+        }
+    }
+
+    return parseInt(strength);
+}
+
