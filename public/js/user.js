ea905757545ed66f1b349087ea47be3a175cf7e3
Adam Roddick <adamroddick.developer@gmail.com>
2023-10-20 13:59:18 +1300

    Modify users.js with followers

--- public/js/user.js
+++ public/js/user.js
@@ -1,8 +1,20 @@
 class User {
-    constructor(id, username, email, password) {
+    constructor(id, username, email, password, following) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.following = following || [];
+    }
+
+    followUser(user) {
+        this.following.push(user);
+    }
+
+    unfollowUser(user) {
+        const index = this.following.indexOf(user);
+        if (index !== -1) {
+            this.following.splice(index, 1);
+        }
     }
 }
