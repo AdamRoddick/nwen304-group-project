b1d1ff8e3192b78202bf4cc761e0e6fb3748ef1f
Benjamin <benmcevoy2002@gmail.com>
2023-10-21 01:17:15 +1300

    initialisation of variables for authentication

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,31 @@
+// Firebase
 const functions = require('firebase-functions');
 
 const http = require('http');
 const express = require('express');
 const app = express();
 
-//const sessions = require('express-session');
+// For parsing authentcation requests
+const bodyParser = require('body-parser');
+
+// For security 
+// Cross Site Request Forgery
+const csrf = require('csurf');
+
+// For sessions and cookies
+const sessions = require('express-session');
+const cookieParser = require('cookie-parser');
+
 const { initializeApp } = require('firebase-admin/app');
-
-
-const path = require('path');
 const ejs = require('ejs');
 
-
-const port = process.env.PORT || 3000; // Use the specified port or 3000 by default
-
 // Define the path to your static files (CSS, JavaScript, images, etc.)
+const path = require('path');
 const publicDirectoryPath = path.join(__dirname, '/public');
-
 // Serve static files from the 'public' directory
 app.use(express.static(publicDirectoryPath));
-
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-
 // Define a route to handle requests for your home page
 app.get('/', (req, res) => {
     res.render('index', { title: 'OurSpace' });
