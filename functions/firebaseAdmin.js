d5d65a96407017c8170c4c3fe71e5e1deef53ad6
Adam Roddick <adamroddick.developer@gmail.com>
2023-10-23 18:58:07 +1300

    Revert "Merge Functioning login into main"

--- functions/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("../serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://nwen304-groupproject-9db15-default-rtdb.asia-southeast1.firebasedatabase.app"
-});
-
-module.exports = admin;
--- public/login-page/login-controller.js
+++ public/login-page/login-controller.js
@@ -1,40 +1,37 @@
 window.addEventListener("load", init);
 
 function init() {
-  bindEvents();
+    bindEvents();
+
+    // Initialize the postOperations.posts array with posts from localStorage
+    userOperations.user = JSON.parse(localStorage.getItem('users')) || [];
 }
 
+//let auto = autoGen();
+
 function bindEvents() {
-  document.querySelector('#login-form').addEventListener('submit', loginUser);
+    document.querySelector('#login-form').addEventListener('submit', loginUser);
 }
 
 function loginUser(event) {
-  event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); // Prevent the default form submission behavior
 
-  const username = document.querySelector('#loginUsername').value;
-  const password = document.querySelector('#loginPassword').value;
+    const username = document.querySelector('#loginUsername').value;
+    const password = document.querySelector('#loginPassword').value;
 
-  // Make an AJAX request to the server's login API endpoint
-  fetch('/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username, password }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        window.location.href = '/'; // Successful login, redirect to home page
-      } else {
-        alert('Invalid username or password'); // Display an error message
-      }
-    })
-    .catch(error => {
-      console.error('Error during login:', error);
-    });
+    // Check if the email and password match a user in localStorage
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        // Login successful, redirect to index.html
+        window.location.href = '/';
+    } else {
+        // Login failed, display error message
+        alert('Invalid username or password');
+    }
 }
-
 
 // Call the function to display existing posts when the page loads
 window.addEventListener("load", () => {
